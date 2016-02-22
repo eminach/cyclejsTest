041920da30bd6308ac364ec5098450195c31ebbf
Emin <eminach@gmail.com>
2016-02-22 05:55:13 +0400

    check again

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+import Rx from 'rx';
+import Cycle from '@cycle/core';
+import CycleDOM from '@cycle/dom';
 
-},{}]},{},[1]);
+function main(sources) {
+  return {
+    DOM: sources.DOM.select('.myinput').events('input')
+      .map(ev => ev.target.value)
+      .startWith('')
+      .map(name =>
+        div([
+          label('Name:'),
+          input('.myinput', {attributes: {type: 'text'}}),
+          hr(),
+          h1(`Hello ${name}`)
+        ])
+      )
+  };
+}
+
+Cycle.run(main, {
+  DOM: makeDOMDriver('#app')
+});
